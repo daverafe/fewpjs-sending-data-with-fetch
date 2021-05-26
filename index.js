9895ffe4377fc6858529cd9bb9556f0082644e67
David Rafe <david.rafe@gmail.com>
2021-05-26 16:01:17 -0400

    commit tree 4a1c4aaf4be53134917efa78c50d88632d85e468

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+            .then(function(response) {
+                return response.json()
+            })
+            .then(function(object) {
+                const id = object['id']
+                document.body.append(id)
+            })
+            .catch(function(error) {
+                const errorMessage = error.message
+                document.body.append(errorMessage)
+            })
+}
